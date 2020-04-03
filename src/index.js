ad9f08be18c427f9bed7d307ed56bcccc5b0ae85
nickzaf95 <nicholaszafiriou@gmail.com>
2020-04-03 13:14:03 +0100

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 console.log('%c HI', 'color: firebrick')
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+
+fetch(imgUrl)
+    .then( response => response.json())
+    .then( images => displayImages(images))
+
+const displayImages = images.forEach(image => {
+    const area = document.querySelector("#dog-image-container")
+    const listItem = document.createElement("li")
+    listItem.innerText = image.src  
+    area.append(listItem)   
+});
+
